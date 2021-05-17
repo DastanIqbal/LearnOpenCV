808921cb24dfebea012d8382c527f6ec55a88cb5
Iqbal Ahmed <ask2iqbal@gmail.com>
2021-05-17 12:49:16 +0400

    Create intro.py

---
+++ src/02/intro.py
@@ -0,0 +1,18 @@
+import numpay as np
+import cv2
+
+#Read opencv.png logo file from local
+img = cv2.imread("opencv.png") 
+
+#Create Window
+cv2.namedWindow("Logo",cv2.WINDOW_NORMAL)
+
+#Show local opencv.png in window
+cv2.imshow("Logo",img)
+
+#Wait user key press
+cv2.waitKey(0)
+
+#Deep copy the opencv.png to output.png in local
+cv2.imwrite("output.jpg",img)
+exit()
