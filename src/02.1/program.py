c647c4ec01488953aa8e7e80c178ded596b3da88
Iqbal Ahmed <iqbal.ahmed@xische.com>
2021-05-19 00:57:36 +0400

    formated and configure in ide

--- src/02.1/program.py
+++ src/02.1/program.py
@@ -1,18 +1,18 @@
 import numpy as np
 import cv2
 
-#Read opencv.png logo file from local
-img = cv2.imread("./../res/opencv.png") 
+# Read opencv.png logo file from local
+img = cv2.imread("./../res/opencv.png")
 
-#Create Window
-cv2.namedWindow("Logo",cv2.WINDOW_NORMAL)
+# Create Window
+cv2.namedWindow("Logo", cv2.WINDOW_NORMAL)
 
-#Show local opencv.png in window
-cv2.imshow("Logo",img)
+# Show local opencv.png in window
+cv2.imshow("Logo", img)
 
-#Wait user key press
+# Wait user key press
 cv2.waitKey(0)
 
-#Deep copy the opencv.png to output.png in local
-cv2.imwrite("output.jpg",img)
+# Deep copy the opencv.png to output.png in local
+cv2.imwrite("output.jpg", img)
 exit()
