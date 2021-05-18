1814f7a5041ac3832fb4a6851c28bef6e8217828
Iqbal Ahmed <iqbal.ahmed@xische.com>
2021-05-19 00:57:36 +0400

    formated and configure in ide

--- src/02.2/program.py
+++ src/02.2/program.py
@@ -1,37 +1,34 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("./../res/opencv-logo.png",1) #1 is a color channel BGR
+img = cv2.imread("./../res/opencv-logo.png", 1)  # 1 is a color channel BGR
 
 print("*****Show image color(BGR) channel One data*****")
-print(img) #Print image data in array and type
-
+print(img)  # Print image data in array and type
 
 print("\n*****Image data type*****")
-print(type(img)) #Print image type 
+print(type(img))  # Print image type
 
 print("\n*****Number of rows in Image*****")
-print(len(img)) #Length of Image, number of rows in array
+print(len(img))  # Length of Image, number of rows in array
 
 print("\n*****Number of vertical column on top row in Image*****")
-print(len(img[0])) # top row, vertical column
+print(len(img[0]))  # top row, vertical column
 
 print("\n*****Number of color channel in Image*****")
-print(len(img[0][0])) #no of channel 
+print(len(img[0][0]))  # no of channel
 
 print("\n*****Get all the row,column and color channel from Image*****")
-print(img.shape) #print number of row,col and channel
+print(img.shape)  # print number of row,col and channel
 
 print("\n*****Image data type*****")
-print(img.dtype) # data type of image, uint8 means max 2 pow 8 value possible in array of index
-
+print(img.dtype)  # data type of image, uint8 means max 2 pow 8 value possible in array of index
 
 print("\n*****Get pixel value at 10th row and 5th column from Image*****")
-print(img[10, 5]) #10 row 5 th col , will get pixel value 
+print(img[10, 5])  # 10 row 5 th col , will get pixel value
 
 print("\n*****Get one channel of Image*****")
-print(img[:, :, 0]) #one channel image
-
+print(img[:, :, 0])  # one channel image
 
 print("\n*****Total number of row x column in Image*****")
-print(img.size) #total number of column
+print(img.size)  # total number of column
--- src/02.3/program.py
+++ src/02.3/program.py
@@ -1,32 +1,31 @@
-import numpy as np
 import cv2
+import numpy as np
 
-#Intialize array with 0 value
+# Intialize array with 0 value
 black = np.zeros([150, 200, 1], 'uint8')
 cv2.imshow("Black", black)
 print(black[0, 0, :])
 
 cv2.waitKey(0)
 
-
-#Intialize array with ones value
+# Intialize array with ones value
 ones = np.ones([150, 200, 3], 'uint8')
 cv2.imshow("Ones", ones)
 print(ones[0, 0, :])
 
 cv2.waitKey(0)
 
-#Intialize array with ones and fill with max value
+# Intialize array with ones and fill with max value
 white = np.ones([150, 200, 3], 'uint8')
-#white *= (2**8-1)
+# white *= (2**8-1)
 white[:, :] = (255, 255, 255)
 cv2.imshow("White", white)
 print(white[0, 0, :])
 
 cv2.waitKey(0)
 
-#Intalize array with blue color
-color = ones.copy() #Deap copy
+# Intalize array with blue color
+color = ones.copy()  # Deap copy
 color[:, :] = (0, 255, 0)
 cv2.imshow("Green", color)
 
