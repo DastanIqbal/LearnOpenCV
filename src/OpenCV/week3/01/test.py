9a98e23cb66043f33048e7f4bd60eb61bd49d253
Iqbal Ahmed <iqbal.ahmed@xische.com>
2021-08-13 11:18:18 +0400

    course: finished thresholding

--- src/OpenCV/week3/01/test.py
+++
@@ -1,9 +0,0 @@
-from dataPath import DATA_PATH
-
-print(DATA_PATH)
-# import cv2
-# im = cv2.imread(DATA_PATH+"/boy.jpg")
-# cv2.imshow("Image",im)
-# print(DATA_PATH)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
